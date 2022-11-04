66e38d81c821d42ac5a4c429a6fa2c98f5e28be9
Talukanyani Mutshaeni <tmutshaeni@hotmail.com>
2022-11-04 20:34:08 +0200

    skills page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import styles from './App.module.css'
 
 import Home from './components/Home';
 import Navbar from './components/Navbar'
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className={styles.app_overlay}>
       <div className={styles.app}>
         <Navbar />
-        <Home />
+        {/* <Home /> */}
+        <Skills />
       </div>
     </div>
   );
