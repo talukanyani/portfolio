8c1cc2504c81b761054e2538b7d151b1436736bf
Talukanyani Mutshaeni <tmutshaeni@hotmail.com>
2022-11-06 23:02:58 +0200

    css prefixing and projects edit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,7 @@ export default function Projects() {
     }
 
     return (
-        <main className={styles.projects}>
+        <div className={styles.projects}>
             <h1>My Projects</h1>
             <nav>
                 <button
@@ -63,7 +63,7 @@ export default function Projects() {
             {currentTab === 0 && <Project1 />}
             {currentTab === 1 && <Project2 />}
             {currentTab === 2 && <Project3 />}
-        </main>
+        </div>
     )
 }
 
@@ -71,8 +71,9 @@ function Project1() {
     return (
         <main>
             <h2>Tmlab Website</h2>
+
             <p>
-                I have developed a website to show my future apps. I branded it Tmlab which stands for Talukanyani Mutshaeni's Laboratry.
+                I developed a website to show my future apps. I branded it Tmlab which stands for Talukanyani Mutshaeni's Laboratry. Users can find links to download specific app in Play Store or App Store. Users can send messages to Tmlab and also subscribe to their newsletter.
             </p>
 
             <ul>
@@ -80,10 +81,10 @@ function Project1() {
                     I coded with <mark>HTML</mark> and <mark>CSS</mark> for structuring, positioning and styling. I also used CSS to make a website responsive to all screen sizes.
                 </li>
                 <li>
-                    I programed with <mark>JavaScript</mark> and <mark>React</mark> for best functionality and for all user iteractions.
+                    I programed with <mark>JavaScript</mark> and <mark>React</mark> for best user interface and user experience.
                 </li>
                 <li>
-                    I created a server program using <mark>Node.js</mark>, <mark>Express</mark>, and <mark>JavaScript</mark> to serve web pages to the user's browser, and to handle other user's request such as sending message. The program send back the response using REST API.
+                    I created a server program using <mark>Node.js</mark>, <mark>Express</mark>, and <mark>JavaScript</mark> to serve web pages to the user's browser, and to handle other user's request such as sending message.
                 </li>
                 <li>
                     I integrated the server program with <mark>MySQL</mark> to store and retrive user's data.
@@ -92,30 +93,54 @@ function Project1() {
                     I deployed on <mark>AWS</mark> cloud, computing with Elastic Beanstalk, Route53 and Amazon Relational Database.
                 </li>
             </ul>
+
             <div className={styles.project_links}>
                 <p>
-                    You can find the code for this project from my <a href='https://github.com/Talukanyani'>GitHub account</a> or using links below.
+                    You can find the code for this project from my{' '}
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://github.com/Talukanyani'
+                    >
+                        GitHub account
+                    </a>
+                    {' '}or using links below.
                 </p>
                 <ul>
                     <li>
-                        <a href='https://github.com/Talukanyani/tmlab-website-client'>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href='https://github.com/Talukanyani/tmlab-website-client'
+                        >
                             Tmlab Website Client Repository
                         </a>
                     </li>
                     <li>
-                        <a href='https://github.com/Talukanyani/tmlab-website-server'>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href='https://github.com/Talukanyani/tmlab-website-server'
+                        >
                             Tmlab Website Server Repository
                         </a>
                     </li>
                 </ul>
-
                 <p>
-                    This project is available online at <a href='https://tmlab.tech'>tmlab.tech</a>
+                    This project is available online at{' '}
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://tmlab.tech'
+                    >
+                        tmlab.tech
+                    </a>
                 </p>
-                <section>
-                    <img src={web_screenshoot} alt='Tmlab Website' />
-                </section>
             </div>
+
+            {/* <section className={styles.project_picture}>
+                <img src={web_screenshoot} alt='Tmlab Website' />
+            </section> */}
         </main>
 
     )
@@ -125,27 +150,42 @@ function Project2() {
     return (
         <main>
             <h2>This Website</h2>
+
             <p>
-                I developed this website to show my skills, creativity, and projects.
+                I designed and developed this website which is in your face right now.
             </p>
+
             <ul>
                 <li>
-                    I hard coded with <mark>HTML</mark> and <mark>CSS</mark> for structuring, positioning and styling. I also used CSS to make a website responsive to screen sizes of 480px and below.
+                    I coded with <mark>HTML</mark> and <mark>CSS</mark> for structuring, positioning, styling, and for responsive design.
                 </li>
                 <li>
-                    I programed with <mark>JavaScript</mark> and <mark>React</mark> for best functionality and for all user iteractions. For example, the iteractions you have made to land on this page was made possible by my JavaScript and React code.
+                    I programed with <mark>JavaScript</mark> and <mark>React</mark> for best functionalty and for all user iteractions.
                 </li>
                 <li>
-                    I used <mark>REST API</mark> for user's resquest. When you send message from this website, your request will be resolved by tmlab server program.
+                    I deployed this website on GitHub pages and used <mark>REST API</mark> to handle user's request such as sending message.
                 </li>
             </ul>
+
             <div className={styles.project_links}>
                 <p>
-                    You can find the code for this project from my <a href='https://github.com/Talukanyani'>GitHub account</a> or using the link below.
+                    You can find the code for this project from my{' '}
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://github.com/Talukanyani'
+                    >
+                        GitHub account
+                    </a>
+                    {' '}or using the link below.
                 </p>
                 <ul>
                     <li>
-                        <a href='https://github.com/Talukanyani/my_portfolio'>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href='https://github.com/Talukanyani/my_portfolio'
+                        >
                             My Portfolio Repository
                         </a>
                     </li>
@@ -167,12 +207,24 @@ function Project3() {
             </ul>
             <div className={styles.project_links}>
                 <p>
-                    You can find the code for this project from my <a href='https://github.com/Talukanyani'>GitHub account</a> or using the link below.
+                    You can find the code for this project from my{' '}
+                    <a
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://github.com/Talukanyani'
+                    >
+                        GitHub account
+                    </a>
+                    {' '}or using the link below.
                 </p>
                 <ul>
                     <li>
-                        <a href='https://github.com/Talukanyani/sc_app'>
-                            SC APP Repository
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href='https://github.com/Talukanyani/sc_app'
+                        >
+                            SC App Repository
                         </a>
                     </li>
                 </ul>
